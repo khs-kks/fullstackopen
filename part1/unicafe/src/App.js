38503319059c1e40a526a12508b3f631ffe0aebf
khs-kks <95934427+khs-kks@users.noreply.github.com>
2023-05-31 21:16:49 +0200

    Remove comments

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,40 +1,5 @@
 import { useState } from 'react'
 
-// TODO STATISTIC LINE Exercise 1.10: unicafe step5
-
-// const StatisticLine = (props) => {
-//   if (props.text === "positive") {
-//     return (
-//       <p>{props.text} {props.value}%</p>
-//     )
-//   }
-//   return (
-//     <p>{props.text} {props.value}</p>
-//   )
-// }
-
-// const Statistics = (props) => {
-
-//   if (props.stats.all === 0) {
-//     return (
-//       <p>no feedback yet</p>
-//     )
-//   }
-
-//   return (
-//     <>
-//       <h1>statistics</h1>
-//       <StatisticLine text="good" value={props.stats.printGood} />
-//       <StatisticLine text="bad" value={props.stats.printBad} />
-//       <StatisticLine text="neutral" value={props.stats.printNeutral} />
-//       <StatisticLine text="total" value={props.stats.total} />
-//       <StatisticLine text="average" value={props.stats.average} />
-//       <StatisticLine text="positive" value={props.stats.positive} />
-//     </>
-//   )
-
-// }
-
 const StatisticLine = (props) => {
   if (props.text === "positive") {
     return (
