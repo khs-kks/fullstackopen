8a0948f3948144a8e43fcd01b2c71ac61103a24b
khs-kks <95934427+khs-kks@users.noreply.github.com>
2023-05-31 14:47:26 +0200

    Complete exercise 1.7 unicafe step2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -32,6 +32,10 @@ const App = () => {
   const printBad = bad;
   const printNeutral = neutral;
 
+  const all = good + bad + neutral;
+  const average = (good - bad) / all;
+  const positive = (good / all) * 100
+  
   return (
     <div>
       <h1>give feedback</h1>
@@ -42,7 +46,9 @@ const App = () => {
       <p>good {printGood}</p>
       <p>bad {printBad}</p>
       <p>neutral {printNeutral}</p>
-
+      <p>total {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive}%</p>
     </div>
   )
 }
