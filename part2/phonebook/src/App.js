3832008b4ea97f2031ad03e9b79f55175a8cae07
khs-kks <95934427+khs-kks@users.noreply.github.com>
2023-06-02 21:47:04 +0200

    Complete 2.11: The Phonebook Step6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,15 +1,21 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
+import axios from 'axios';
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchTerm, setSearchTerm] = useState('')
 
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
   const handleSubmit = (event) => {
     event.preventDefault()
 
